c339790b6e152fac0224e58937b3aab76191be85
sqykly <sqykly@gmail.com>
2018-03-19 17:40:39 -0400

    Add files via upload

---
+++ valuenetwork/valueaccounting/tests/test_resource_types.py
@@ -0,0 +1,40 @@
+# Verify that the response's new resource form contains no quantity field
+
+from django.test import TestCase, Client
+from valuenetwork.valueaccounting.models import EconomicResourceType
+from xml.etree import ElementTree
+from exceptions import Exception
+
+
+class NoQuantityTest(TestCase):
+
+    def setUp(self):
+        self.rt = EconomicResourceType(
+            name='Spam',
+            slug='test-rt-spam'
+        )
+        self.rt.save()
+
+    def tearDown(self):
+        self.rt.delete()
+
+    def test_no_quantity(self):
+        client = Client()
+        resp = client.get(
+            '/accounting/resource-type/%s/' % self.rt.id,
+            follow=True
+        )
+        doc = ElementTree.fromstring(resp.content)
+
+        res_form = None
+        for form in doc.iter('form'):
+            node_id = form.get('id')
+            if node_id == 'resourceForm':
+                res_form = form
+                break
+
+        if not res_form:
+            raise Exception((_('Could not find resource form in resource type form'),))
+
+        for inp in res_form.iter('input'):
+            assert inp.get('name') != 'quantity'
---
+++ valuenetwork/valueaccounting/tests/test_transfers.py
@@ -0,0 +1,81 @@
+
+from django.test import TestCase
+from valuenetwork.valueaccounting.models import Transfer, EventType, EconomicEvent, EconomicResourceType
+from datetime import date
+from decimal import Decimal
+from exceptions import Exception, AssertionError
+
+class NoThrowTransferText(TestCase):
+
+    def setUp(self):
+        self.transfer = Transfer(
+            created_date=date.today(),
+            slug='test-xfer-xfer',
+            transfer_date=date.today()
+        )
+        self.transfer.save()
+
+        self.et_give = EventType(
+            name='Give',
+            label='do not care',
+            resource_effect='=',    # don't care
+            slug='test-xfer-et-give'
+        )
+        self.et_give.save()
+
+        self.et_recv = EventType(
+            name='Receive',
+            label='do not care',
+            resource_effect='=',    # don't care
+            slug='test-xfer-et-recv'
+        )
+        self.et_recv.save()
+
+        self.rt_spam = EconomicResourceType(
+            name='Spam',
+            slug='test-xfer-rt-spam'
+        )
+        self.rt_spam.save()
+
+        self.rt_eggs = EconomicResourceType(
+            name='Eggs',
+            slug='test-xfer-rt-eggs'
+        )
+        self.rt_eggs.save()
+
+        # both events should have null agents so that there is no extra
+        # assignment to give_text or receive_text
+        self.ev_give = EconomicEvent(
+            event_type=self.et_give,
+            event_date=date.today(),
+            resource_type=self.rt_spam,
+            quantity=Decimal('1.0'),
+            transfer=self.transfer,
+            slug='test-xfer-ev-give'
+        )
+        self.ev_give.save()
+
+        self.ev_recv = EconomicEvent(
+            event_type=self.et_recv,
+            event_date=date.today(),
+            resource_type=self.rt_eggs,
+            quantity=Decimal('1.0'),
+            transfer=self.transfer,
+            slug='test-xfer-ev-recv'
+        )
+        self.ev_recv.save()
+
+    def tearDown(self):
+        self.ev_recv.delete()
+        self.ev_give.delete()
+        self.rt_eggs.delete()
+        self.rt_spam.delete()
+        self.et_give.delete()
+        self.et_recv.delete()
+        self.transfer.delete()
+
+    def test_xfer_throw(self):
+        try:
+            dont_care = self.transfer.event_text()
+        except Exception as e:
+            raise AssertionError((_('Transfer raised an exception: ' + str(e)),))
