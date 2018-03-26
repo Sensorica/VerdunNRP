18591a3fc59de773804de7d91a88813ec47b48b7
sqykly <sqykly@gmail.com>
2018-03-25 23:22:13 -0400

    initial commit

---
+++ valuenetwork/valueaccounting/templatetags/form_dialog.py
@@ -0,0 +1,27 @@
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag("valueaccounting/_form_dialog.html")
+def form_dialog(
+    dialog_id,
+    action,
+    form,
+    title,
+    confirm_text='Save',
+    method='POST',
+    formset=None,
+    label_id='label-for-%s' % dialog_id,
+    form_id='form-for-%s' % dialog_id
+):
+    return {
+        'dialog_id': dialog_id,
+        'action': action,
+        'form': form,
+        'title': title,
+        'confirm_text': confirm_text,
+        'method': method,
+        'formset': formset,
+        'label_id': label_id,
+        'form_id': form_id
+    }
