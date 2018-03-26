07aa494e37d14ac1e8413d78612b0ceaf5ca08fe
sqykly <sqykly@gmail.com>
2018-03-25 23:23:14 -0400

    specified full path to template

--- valuenetwork/valueaccounting/templatetags/chair_tag.py
+++ valuenetwork/valueaccounting/templatetags/chair_tag.py
@@ -2,7 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('chair.html')
+@register.inclusion_tag('valueaccounting/chair.html')
 def chair(desc, field):
     return {'desc': desc, 'field': field, 'linebreaks': ('\n' in field or '<br' in field)}
 
