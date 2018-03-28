968fbff30ab3852d3dddb20118c8a71b324d6e08
sqykly <sqykly@gmail.com>
2018-03-27 23:31:40 -0400

    Added more server config
    Rest of config will be done with secure source for reasons

--- settings.py
+++ settings.py
@@ -59,8 +59,9 @@ MEDIA_URL = "/site_media/media/"
 # Don"t put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
+#STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
 #STATIC_ROOT = os.path.join(PACKAGE_ROOT, "static")
+STATIC_ROOT = "/home/verdun/webapps/verdunnrp_static/"
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
