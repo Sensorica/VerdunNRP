2d7873efc5c735c044df0702e44745f69ddb17f6
sqykly <sqykly@gmail.com>
2018-04-05 07:26:22 -0400

    Added necessary user & agent info
    My password is in here, please don't look.

--- valuenetwork/valueaccounting/tests/test_resource_types.py
+++ valuenetwork/valueaccounting/tests/test_resource_types.py
@@ -1,7 +1,8 @@
 # Verify that the response's new resource form contains no quantity field
 
 from django.test import TestCase, Client
-from valuenetwork.valueaccounting.models import EconomicResourceType
+from django.contrib.auth.models import User
+from valuenetwork.valueaccounting.models import EconomicResourceType, EconomicAgent, AgentType, AgentUser
 from django_webtest import WebTest
 from exceptions import Exception
 
@@ -17,15 +18,47 @@ class NoQuantityTest(WebTest):
         )
         self.rt.save()
 
+        self.the_best_user = User.objects.create_user('david', 'moc.liamg@gmail.com', 'my actual secret password in real life')
+        self.the_best_user.save()
+
+        self.mi6 = AgentType(
+            name='MI6 Spy',
+            party_type='org',
+            #is_context=True
+        )
+        self.mi6.save()
+
+        self.secret_agent = EconomicAgent(
+            name='bond, james bond',
+            nick='007',
+            agent_type=self.mi6,
+            #is_context=True,
+            slug='bond-james-bond'
+        )
+        self.secret_agent.save()
+
+        self.queen = AgentUser(
+            agent=self.secret_agent,
+            user=self.the_best_user
+        )
+        self.queen.save()
+
+        self.app.set_user(self.the_best_user)
+
     def tearDown(self):
         self.rt.delete()
+        self.queen.delete()
+        self.secret_agent.delete()
+        self.mi6.delete()
+        self.the_best_user.delete()
+        self.set_user(None)
 
     def test_no_quantity(self):
         """
         Resource Type creation form should not take a quantity
         """
         resp = self.app.get(
-            '/accounting/resource-type/%s/' % self.rt.id
+            '/accounting/resource-type/%s/' % (self.rt.id,)
         )#.follow()#again if resp requires a redirect some day.
         res_form = None
         field = None
