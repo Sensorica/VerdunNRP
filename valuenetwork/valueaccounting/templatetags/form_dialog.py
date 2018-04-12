8bd151579a12fc1780a47abdce8e9688f10ad5bf
sqykly <sqykly@gmail.com>
2018-04-12 12:07:13 -0400

    moved default args inside function

--- valuenetwork/valueaccounting/templatetags/form_dialog.py
+++ valuenetwork/valueaccounting/templatetags/form_dialog.py
@@ -11,9 +11,14 @@ def form_dialog(
     confirm_text='Save',
     method='POST',
     formset=None,
-    label_id='label-for-%s' % dialog_id,
-    form_id='form-for-%s' % dialog_id
+    label_id=None,
+    form_id=None
 ):
+    if not label_id:
+        label_id = 'label-for-%s' % (dialog_id,)
+    if not form_id:
+        form_id = 'form-for-%s' % (dialog_id,)
+
     return {
         'dialog_id': dialog_id,
         'action': action,
