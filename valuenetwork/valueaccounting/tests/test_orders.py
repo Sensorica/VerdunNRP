5b89a01bee45e0b90170f085f1bd9e301055168a
sqykly <sqykly@gmail.com>
2018-05-17 13:05:57 -0400

    tweak diagnostic more

--- valuenetwork/valueaccounting/tests/test_orders.py
+++ valuenetwork/valueaccounting/tests/test_orders.py
@@ -201,8 +201,8 @@ class OrderTest(WebTest):
         return resp
 
     def diag_form_fields(self, form):
-        print("seeing form receiver: %s" % (form.fields['receiver'],))
-        print("seeing form exchange_type: %s" % (form.fields['exchange_type'],))
+        print("seeing form receiver: %s" % (form.fields['receiver'].render(),))
+        print("seeing form exchange_type: %s" % (form.fields['exchange_type'].render(),))
 
     def test_create_order(self):
         """Test create_order view
